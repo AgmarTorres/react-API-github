cee32dab776952164413b549d9dc7522e80c7513
Agmar Torres <agmartorres@gmail.com>
2020-04-26 01:54:10 -0300

    Dashboard added into application with CSS

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,8 +1,64 @@
-import React from 'react'
-import { Title } from './styles'
+import React from 'react';
+import { Title, Form, Repositories } from './styles';
+import {  FiChevronRight } from 'react-icons/fi';
+
+
+import logoImg from '../../assets/logo.svg';
 
 const Dashboard: React.FC = () =>{
-    return <Title> DashBoard</Title>
+
+  return (
+    <>
+      <img src={ logoImg} alt ="GitHub Explorer" />
+      <Title> DashBoard</Title>
+      <Form action="" >
+        <input type="text" placeholder="Digite o nome do respositorio"/>
+        <button type="submit">Pesquisar</button>
+      </Form>
+      <Repositories>
+        <a href="teste" className="href">
+          <img
+            src="https://avatars3.githubusercontent.com/u/29600618?s=460&u=e4ef9bd7be29e304fa0c6fecd6ab5c61ccf130cb&v=4"
+            alt=""
+          />
+          <div>
+            <strong>RocketSeat</strong>
+            <p>Easy peasy gighly scalable ReactJS</p>
+
+          </div>
+          <FiChevronRight size = {20} />
+        </a>
+
+
+        <a href="teste" className="href">
+          <img
+            src="https://avatars3.githubusercontent.com/u/29600618?s=460&u=e4ef9bd7be29e304fa0c6fecd6ab5c61ccf130cb&v=4"
+            alt=""
+          />
+          <div>
+            <strong>RocketSeat</strong>
+            <p>Easy peasy gighly scalable ReactJS</p>
+
+          </div>
+          <FiChevronRight size = {20} />
+        </a>
+
+
+        <a href="teste" className="href">
+          <img
+            src="https://avatars3.githubusercontent.com/u/29600618?s=460&u=e4ef9bd7be29e304fa0c6fecd6ab5c61ccf130cb&v=4"
+            alt=""
+          />
+          <div>
+            <strong>RocketSeat</strong>
+            <p>Easy peasy gighly scalable ReactJS</p>
+
+          </div>
+          <FiChevronRight size = {20} />
+        </a>
+      </Repositories>
+    </>
+  )
 }
 
 export default Dashboard;
